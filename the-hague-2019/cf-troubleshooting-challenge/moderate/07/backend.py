c1293215f6e022ad8b26443e3f5bcfadc46332a1
Sourcery AI <>
2022-08-02 19:18:50 +0000

    'Refactored by Sourcery'

--- the-hague-2019/cf-troubleshooting-challenge/moderate/07/backend.py
+++ the-hague-2019/cf-troubleshooting-challenge/moderate/07/backend.py
@@ -8,42 +8,37 @@ gcp_url = 'https://status.cloud.google.com/'
 aws_url = 'https://status.aws.amazon.com/'
 
 # helper, who gets URL content as html
-def get_html (url):
+def get_html(url):
 
    r = requests.get(url)
-   tree = html.fromstring(r.text)
-
-   return tree
+   return html.fromstring(r.text)
 
 app = Flask(__name__)
 
 @app.route("/")
 def greeting():
-    text = "<h1 allign=center>This is test application, backend part<h1><p>App will curl AWS and GCP status page and check, if services available</p><p>/aws_eu, /gcp, /status calls available</p>"
-    return text
+   return "<h1 allign=center>This is test application, backend part<h1><p>App will curl AWS and GCP status page and check, if services available</p><p>/aws_eu, /gcp, /status calls available</p>"
 
 @app.route("/aws_eu")
 def aws_status():
-    output = {}
-    tree = get_html(aws_url)
-    status_aws_europe_div = tree.xpath('//div[contains(@id, "EU_block")]')[0]
-    status_aws_europe_table = status_aws_europe_div.xpath('.//table')[0]
-    status_aws_europe= status_aws_europe_table.xpath('.//td[@colspan]')[0].text
-
-    if status_aws_europe :
-        output['status'] = status_aws_europe
-        output['error'] = ''
-    else:
-        output['error'] = 'Something wrong with AWS status page. Check %s', aws_url
-    return output
+   output = {}
+   tree = get_html(aws_url)
+   status_aws_europe_div = tree.xpath('//div[contains(@id, "EU_block")]')[0]
+   status_aws_europe_table = status_aws_europe_div.xpath('.//table')[0]
+   if status_aws_europe := status_aws_europe_table.xpath(
+       './/td[@colspan]')[0].text:
+      output['status'] = status_aws_europe
+      output['error'] = ''
+   else:
+      output['error'] = 'Something wrong with AWS status page. Check %s', aws_url
+   return output
 
 @app.route("/status")
 def backend_status():
-    output = {}
-    output['instance_id'] = os.getenv('CF_INSTANCE_INDEX')
-    output['instance_guid'] = os.getenv('CF_INSTANCE_GUID')
-    output['instance_port'] = os.getenv('CF_INSTANCE_PORT')
-    return output
+   output = {'instance_id': os.getenv('CF_INSTANCE_INDEX')}
+   output['instance_guid'] = os.getenv('CF_INSTANCE_GUID')
+   output['instance_port'] = os.getenv('CF_INSTANCE_PORT')
+   return output
 
 @app.route("/gcp")
 def gcp_status():
--- the-hague-2019/cf-troubleshooting-challenge/moderate/07/frontend.py
+++ the-hague-2019/cf-troubleshooting-challenge/moderate/07/frontend.py
@@ -8,26 +8,24 @@ backend_url = os.getenv('BACKEND_URL')
 
 # helper, who gets URL content as html
 
-def get_request (url):
+def get_request(url):
 
    r = requests.get(url)
-   tree = html.fromstring(r.text)
-
-   return tree
+   return html.fromstring(r.text)
 
 app = Flask(__name__)
 
 @app.route("/")
 def greeting():
 
-    gcp = requests.get(backend_url + '/gcp').text
-    gcp_json = json.loads(gcp)
-    aws = requests.get(backend_url + '/aws_eu').text
-    aws_json = json.loads(aws)
-    backend = requests.get(backend_url + '/status').text
-    backend_json = json.loads(backend)
-     
-    return render_template('index.html', gcp_date=gcp_json['date'], gcp_status=gcp_json['state'], aws_status=aws_json['status'], backend_id=backend_json['instance_id'], backend_guid=backend_json['instance_guid'], backend_port=backend_json['instance_port']) 
+   gcp = requests.get(f'{backend_url}/gcp').text
+   gcp_json = json.loads(gcp)
+   aws = requests.get(f'{backend_url}/aws_eu').text
+   aws_json = json.loads(aws)
+   backend = requests.get(f'{backend_url}/status').text
+   backend_json = json.loads(backend)
+
+   return render_template('index.html', gcp_date=gcp_json['date'], gcp_status=gcp_json['state'], aws_status=aws_json['status'], backend_id=backend_json['instance_id'], backend_guid=backend_json['instance_guid'], backend_port=backend_json['instance_port']) 
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = os.getenv('PORT') ,debug = True)
